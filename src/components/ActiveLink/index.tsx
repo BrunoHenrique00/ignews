783ba4b311b98dcdbb9c48a4d3b939330f855c48
Bruno Henrique <bh.bsb2011@hotmail.com>
2022-03-09 22:04:48 -0300

    fix TS type

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -5,7 +5,6 @@ import { cloneElement, ReactElement } from 'react'
 interface ActiveLinkProps extends LinkProps {
     children: ReactElement
     activeClassName: string
-    href?: string
 }
 
 export function ActiveLink({children , activeClassName, ...rest}: ActiveLinkProps){
